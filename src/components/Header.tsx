c1d921a07fa4e2662e7467663aef1010db3f69a6
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-05-27 15:06:39 -0300

    styles: app layout header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import logoSvg from '../assets/logo.svg'
+import logoutSvg from '../assets/logout.svg'
+
+export function Header() {
+	return (
+		<header className="w-full flex justify-between px-3 md:px-22 xl:max-w-[1185px]">
+			<img src={logoSvg} alt="Logo. Refund" className="my-8" />
+
+			<div className="flex items-center gap-3">
+				<span className="text-sm font-semibold text-gray-200">Olá, Thiago</span>
+
+				<img
+					src={logoutSvg}
+					alt="Ícone. Deslogar"
+					className="mt-9 mb-7 cursor-pointer hover:opacity-75 transition ease-linear"
+				/>
+			</div>
+		</header>
+	)
+}
--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router'
-import logoSvg from '../../assets/logo.svg'
+import { Header } from '../Header'
 
 export function AppLayout() {
 	return (
 		<div className="w-screen h-screen bg-gray-400 flex flex-col items-center text-gray-100">
-			<header>
-				<img src={logoSvg} alt="Logo. Refund" />
-			</header>
+			<Header />
 			<main className="p-3 w-full md:w-auto">
 				<Outlet />
 			</main>
