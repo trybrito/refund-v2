3f78b7e24fab1fa4c1822dc50c10b2dc9d61707b
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-05-27 21:15:36 -0300

    styles: confirm page

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,4 +1,5 @@
 import { Navigate, useLocation } from 'react-router'
+import okSvg from '../assets/ok.svg'
 
 export function Confirm() {
 	const location = useLocation()
@@ -8,8 +9,24 @@ export function Confirm() {
 	}
 
 	return (
-		<>
-			<h1>Confirm page</h1>
-		</>
+		<div className="bg-gray-500 lg:w-[512px] rounded-xl flex flex-col items-center p-10 gap-6">
+			<h1 className="text-2xl font-bold text-center text-green-100">
+				Solicitação enviada!
+			</h1>
+
+			<img src={okSvg} alt="Ícone de OK" className="w-28" />
+
+			<p className="text-sm text-gray-200 text-center">
+				Agora é apenas aguardar! Sua solicitação será processada e, em breve, o
+				setor Financeiro entrará em contato com você.
+			</p>
+
+			<a
+				href="/"
+				className="w-full p-[15px] text-center text-sm bg-green-100 font-bold rounded-lg text-white hover:bg-green-200 transition ease-linear mt-4"
+			>
+				Nova solicitação
+			</a>
+		</div>
 	)
 }
