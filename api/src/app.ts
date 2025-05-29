c3ed31fb703eaec682a4cf941e68e7093c9c7908
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-05-29 01:05:22 -0300

    feat: find purchase receipt

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors'
 import cors from 'cors'
 import express from 'express'
 
+import { uploadConfig } from './configs/upload'
 import { errorHandling } from './middlewares/error-handling'
 import { routes } from './routes'
 
@@ -10,7 +11,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use(routes)
+app.use('/uploads', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(errorHandling)
 
 export { app }
